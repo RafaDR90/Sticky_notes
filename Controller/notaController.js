2e0f682c0e49cf7bf9c391a27a7ff8cb5b7d95c9
Rafa <rafa18220delgado@gmail.com>
2023-11-12 22:36:19 +0100

    pamñn

--- Controller/notaController.js
+++ Controller/notaController.js
@@ -1,3 +1,52 @@
-window.onload=()=>{
-    
-}
+window.onload = () => {
+    var id = 0;
+    const NewBlueSticky = document.getElementById('createNoteBlue');
+    const NewYellowSticky = document.getElementById('createNoteYellow');
+    const blueSticky = document.querySelector('#BlueContainer');
+    const yellowSticky = document.querySelector('#yellowContainer');
+    const main = document.querySelector('main');
+    var pulsado = false;
+    var cordXresta = 0;
+    var cordYResta = 0;
+    var imagen;
+
+    const agregarSticky = (trigger, stickyContainer) => {
+        let visibility = window.getComputedStyle(stickyContainer).visibility;
+        if (visibility == 'hidden') {
+            stickyContainer.style.visibility = 'visible';
+            stickyContainer.addEventListener('click', (ev) => {
+                pulsado = !pulsado;
+                imagen = stickyContainer;
+                posicionImg = imagen.getBoundingClientRect();
+                cordXresta = ev.clientX - posicionImg.left;
+                cordYResta = ev.clientY - posicionImg.top;
+            });
+        } else {
+            let nuevoSticky = stickyContainer.cloneNode(true);
+            nuevoSticky.id = `contenedor${id++}`;
+            nuevoSticky.style.top = '20%';
+            nuevoSticky.style.left = '83%';
+            main.appendChild(nuevoSticky);
+
+            nuevoSticky.addEventListener('click', (event) => {
+                pulsado = !pulsado;
+                imagen = event.currentTarget;  // Utilizar el evento actual para obtener el elemento clicado
+                posicionImg = imagen.getBoundingClientRect();
+                cordXresta = event.clientX - posicionImg.left;
+                cordYResta = event.clientY - posicionImg.top;
+            });
+        }
+    };
+
+    NewBlueSticky.addEventListener('click', () => agregarSticky(NewBlueSticky, blueSticky));
+    NewYellowSticky.addEventListener('click', () => agregarSticky(NewYellowSticky, yellowSticky));
+
+    document.addEventListener("mousemove", (e) => {
+        cordX = e.clientX;
+        cordY = e.clientY;
+        if (pulsado && imagen) {
+            imagen.style.top = ((cordY - cordYResta) / window.innerHeight) * 100 + "%";
+            imagen.style.left = ((cordX - cordXresta) / window.innerWidth) * 100 + "%";
+        }
+    });
+};
--- Model/nota.js
+++ Model/nota.js
@@ -1,9 +1,36 @@
-class Note{
-    constructor(text){
-        this.text=text;
-        this.coordY=100;
-        this.coordX=100;
+class Note {
+    constructor(text, coordX = 100, coordY = 100) {
+        this.text = text;
+        this.coordX = coordX;
+        this.coordY = coordY;
     }
 
-    
+    getText() {
+        return this.text;
+    }
+
+    setText(newText) {
+        this.text = newText;
+    }
+
+    getCoordX() {
+        return this.coordX;
+    }
+
+    setCoordX(newCoordX) {
+        this.coordX = newCoordX;
+    }
+
+    getCoordY() {
+        return this.coordY;
+    }
+
+    setCoordY(newCoordY) {
+        this.coordY = newCoordY;
+    }
+
+    toString() {
+        console.log(`Note: ${this.text}, Coordinates: (${this.coordX}, ${this.coordY})`);
+    }
 }
+
