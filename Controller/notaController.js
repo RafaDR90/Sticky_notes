8152812737704fe6b7c7d4d0b5591c7753dae53d
Rafa <rafa18220delgado@gmail.com>
2023-11-14 22:51:24 +0100

    14/11 casa

--- Controller/notaController.js
+++ Controller/notaController.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+    //localStorage.clear();
     const NewBlueSticky = document.getElementById('createNoteBlue');
     const NewYellowSticky = document.getElementById('createNoteYellow');
     const blueSticky = document.querySelector('#BlueContainer');
@@ -13,75 +14,28 @@ window.onload = () => {
         var arrayDeNotas = {
             'notas': []
           };
+    }else{
+        arrayDeNotas.notas = arrayDeNotas.notas.map(nota => new Note(nota.text, nota.coordX, nota.coordY, nota.color, nota.id));
+
     }
     console.log(arrayDeNotas)
 
-    arrayDeNotas.notas.forEach(x => {
-        const nota = new Nota(x.text, x.coordX, x.coordY, x.color, x.id);
-        actualizarNota(nota.getId(),nota.getCoordX(),nota.getCoordY(),nota.getText(),nota.getColor());
-    });
+
+    
+
 
     const cargarNotasDesdeLocalStorage = () => {
         arrayDeNotas.notas.forEach(nota => {
-            const nuevoSticky = blueSticky.cloneNode(true);
-            nuevoSticky.style.visibility = 'visible';
-            nuevoSticky.style.top = nota.getCoordY();
-            nuevoSticky.style.left = nota.getCoordX();
-            main.appendChild(nuevoSticky);
+            let x=nota.coordX;
+            let y=nota.coordY;
+            const { x: xPercentage, y: yPercentage } = convertPixelsToPercentage(x, y);
+            if(nota.getColor()=='blue'){
+                agregarSticky(NewBlueSticky,blueSticky,yPercentage,xPercentage,'blue',false,nota.text,nota.id);
+            }else{
+                agregarSticky(NewYellowSticky,yellowSticky,yPercentage,xPercentage,'yellow',false,nota.text,nota.id);
 
-            let child = nuevoSticky.children;
-            let close = child[2];
-            let edit = child[1];
-            let text = child[0];
-            let parrafo = child[3];
-
-            parrafo.innerText = nota.getText();
-
-            close.addEventListener('click', () => {
-                idToDelete = nota.getId();
-                arrayDeNotas.notas = arrayDeNotas.notas.filter(n => n.id !== idToDelete);
-                nuevoSticky.remove();
-                borrarNota(idToDelete);
-                actualizarLocalStorage();
-            });
-
-            edit.addEventListener('click', (e) => {
-                e.stopPropagation();
-                text.style.visibility = 'visible';
-                text.focus();
-                parrafo.style.visibility = 'hidden';
-
-                text.addEventListener('blur', () => {
-                    text.style.visibility = 'hidden';
-                    parrafo.innerText = text.value;
-                    nota.setText(text.value);
-                    parrafo.style.visibility = 'visible';
-                    actualizarNota(nota.getId(), nota.getCoordX(), nota.getCoordY(), nota.getText());
-                    actualizarLocalStorage();
-                });
-
-                text.addEventListener('keydown', (event) => {
-                    if (event.key === 'Enter' || event.key === 'Escape') {
-                        event.preventDefault();
-                        text.style.visibility = 'hidden';
-                        parrafo.innerText = text.value;
-                        nota.setText(text.value);
-                        parrafo.style.visibility = 'visible';
-                        actualizarNota(nota.getId(), nota.getCoordX(), nota.getCoordY(), nota.getText());
-                        actualizarLocalStorage();
-                    }
-                });
-            });
-
-            nuevoSticky.addEventListener('click', (event) => {
-                pulsado = !pulsado;
-                imagen = event.currentTarget;
-                posicionImg = imagen.getBoundingClientRect();
-                cordXresta = event.clientX - posicionImg.left;
-                cordYResta = event.clientY - posicionImg.top;
-                actualizarNota(nota.getId(), posicionImg.left, posicionImg.top, nota.getText());
-                actualizarLocalStorage();
-            });
+            }
+            
         });
     };
 
@@ -96,20 +50,31 @@ window.onload = () => {
 
     var id = arrayDeNotas.notas.length;
 //CONTROLADOR
-    const agregarSticky = (boton, stickyContainer,color) => {
+    function agregarSticky  (boton, stickyContainer,posTop='20%',posLeft='83%',color,nueva=true,texto="",oldId=0)  {
             let nuevoSticky = stickyContainer.cloneNode(true);
             nuevoSticky.style.visibility='visible';
-            nuevoSticky.style.top = '20%';
-            nuevoSticky.style.left = '83%';
+            nuevoSticky.style.top = posTop;
+            nuevoSticky.style.left = posLeft;
             main.appendChild(nuevoSticky);
-            let objNote=new Note('','83%','20%',color,id++);
-            arrayDeNotas.notas.push(objNote);
+            if(nueva){
+                var objNote=new Note('','83%','20%',color,id++);
+                arrayDeNotas.notas.push(objNote);
+                var objetoEnJSON = JSON.stringify(arrayDeNotas);
+                localStorage.setItem('saveNotes', objetoEnJSON);
+            }else{
+                var objNote=new Note(texto,posTop,posLeft,color,oldId);
+            }
+            
             
             let child = nuevoSticky.children;
             let close=child[2];
             let edit=child[1];
             let text=child[0];
             let parrafo=child[3];
+            
+            if(!nueva){
+                parrafo.innerText=texto
+            }
 
             close.addEventListener('click',(e)=>{
                 idToDelete=objNote.getId();
@@ -134,7 +99,7 @@ window.onload = () => {
                     parrafo.innerText = text.value;
                     objNote.setText(text.value);
                     parrafo.style.visibility='visible';
-                    actualizarNota(objNote.getId(),objNote.getCoordX(),objNote.getCoordY(),objNote.getText());
+                    actualizarNota(objNote.getId(),objNote.getCoordX(),objNote.getCoordY(),objNote.getText(),objNote.getColor());
                     var objetoEnJSON = JSON.stringify(arrayDeNotas);
                     localStorage.setItem('saveNotes', objetoEnJSON);
                     console.log(objetoEnJSON)
@@ -147,7 +112,7 @@ window.onload = () => {
                         parrafo.innerText = text.value; 
                         objNote.setText(text.value);
                         parrafo.style.visibility='visible';
-                        actualizarNota(objNote.getId(),objNote.getCoordX(),objNote.getCoordY(),objNote.getText());
+                        actualizarNota(objNote.getId(),objNote.getCoordX(),objNote.getCoordY(),objNote.getText(),objNote.getColor());
                         var objetoEnJSON = JSON.stringify(arrayDeNotas);
                         localStorage.setItem('saveNotes', objetoEnJSON);
                         console.log(objetoEnJSON)
@@ -164,7 +129,7 @@ window.onload = () => {
                 posicionImg = imagen.getBoundingClientRect();
                 cordXresta = event.clientX - posicionImg.left;
                 cordYResta = event.clientY - posicionImg.top;
-                actualizarNota(objNote.getId(),posicionImg.left,posicionImg.top,objNote.getText())
+                actualizarNota(objNote.getId(),posicionImg.left,posicionImg.top,objNote.getText(),objNote.getColor())
                 var objetoEnJSON = JSON.stringify(arrayDeNotas);
                 localStorage.setItem('saveNotes', objetoEnJSON);
                 console.log(objetoEnJSON)
@@ -172,8 +137,8 @@ window.onload = () => {
         
     };
 //VISTA
-    NewBlueSticky.addEventListener('click', () => agregarSticky(NewBlueSticky, blueSticky,'blue'));
-    NewYellowSticky.addEventListener('click', () => agregarSticky(NewYellowSticky, yellowSticky,'yellow'));
+    NewBlueSticky.addEventListener('click', () => agregarSticky(NewBlueSticky, blueSticky,'20%','83%','blue'));
+    NewYellowSticky.addEventListener('click', () => agregarSticky(NewYellowSticky, yellowSticky,'20%','83%','yellow'));
 
     document.addEventListener("mousemove", (e) => {
         cordX = e.clientX;
@@ -185,18 +150,34 @@ window.onload = () => {
             imagen.style.left = posLeft;
         }
     });
-//CONTROLADOR
-    const actualizarNota = (id, newCoordX, newCoordY, newText,color) => {
-    const notaToUpdate = arrayDeNotas.notas.find(nota => nota.id === id);
+
+
+    //CONTROLADOR
+const actualizarNota = (id, newCoordX, newCoordY, newText,color) => {
+    const notaToUpdate = arrayDeNotas.notas.find(nota => nota.id == id);
         if (notaToUpdate) {
             notaToUpdate.setCoordX(newCoordX);
             notaToUpdate.setCoordY(newCoordY);
             notaToUpdate.setText(newText);
             notaToUpdate.setColor(color);
+            var arrayEnJSON =JSON.stringify(arrayDeNotas);
+            localStorage.setItem('saveNotes',arrayEnJSON);
         }
     };
 
     const borrarNota = (id) => {
         arrayDeNotas.notas = arrayDeNotas.notas.filter(nota => nota.id !== id);
     };
+
+
+
+    function convertPixelsToPercentage(x, y) {
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
+    
+        const xPercentage = (x / screenWidth) * 100;
+        const yPercentage = (y / screenHeight) * 100;
+    
+        return { x: `${xPercentage}%`, y: `${yPercentage}%` };
+    }
 };
